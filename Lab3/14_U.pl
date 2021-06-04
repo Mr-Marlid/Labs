25d3cd7cd89e9d07d86c378d81ca456b53e15365
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2021-06-04 15:34:06 +0300

    Delete Lab3 directory

--- Lab3/14_U.pl
+++
@@ -1,9 +0,0 @@
-nevzUp(X,N):-nevz1(X,X,N),!.
-nevz1(_,1,0):-!.
-nevz1(X,Y,N):-Y mod 2 =:= 0,Y1 is Y-1,nevz1(X,Y1,N1),
-    nod(X,Y1,N2), (N2 =:= 1, N is N1 ; N2 =\= 1, N is N1+1);
-    Y mod 2 =\= 0, Y1 is Y-1, nevz(X,Y1,N1), N is N1.
-
-nod(X,X,X) :- !.
-nod(X,Y,A) :- X>Y, !, Z is X-Y, nod(Z,Y,A).
-nod(X,Y,A) :- Z is Y-X, nod(X,Z,A).
